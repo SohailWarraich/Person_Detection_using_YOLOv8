f77e0575405f4ca397bcb84642e379733d521fe4
sohail <arshadsohail092@gmail.com>
2024-05-21 13:44:10 +0500

    person detection using yolov8

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import cv2
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator
+
+## Load YOLOv8-Nano pre-trained model ##
+model = YOLO("yolov8n.pt")
 
 # Initialize Video Capture
 try:
+
     # For Test Sample Video
     cap = cv2.VideoCapture("input_video/video2.mp4")
 except:
-    print("Retry to initiate your IP webcam")
+    print("error")
     raise ConnectionError
+
 # Check to make sure reading video
 assert cap.isOpened(), "Error reading video file"
+
+# Parameters
+class_id = 0  # Class_id 0 is for Person detection only
 
 # While Loop to get frame-by-frame from video
 while cap.isOpened():
@@ -16,6 +26,22 @@ while cap.isOpened():
     if not success:
         print("Video frame is empty or video processing has been successfully completed.")
         break
+
+    # Make model predictions on each frame for specific class_id = 0: person
+    results = model(frame, classes=[class_id])  # Run detection
+
+    # Annotate frame
+    annotator = Annotator(frame)
+    for result in results:
+        for box in result.boxes:
+            bbox = box.xyxy[0].tolist()  # Bounding box
+            class_id = int(box.cls[0])  # Class ID
+            score = box.conf[0]  # Confidence score
+            label = f'{model.names[class_id]} {score:.2f}'
+            annotator.box_label(bbox, label, color=(0, 255, 0))  # Annotate box with label
+
+    # Get annotated frame
+    frame = annotator.result()
 
     # Display frame
     cv2.imshow("Frame", frame)
